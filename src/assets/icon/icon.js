02e76448645a72ad8535a162d8b1b710a354e9c6
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-06 18:57:33 +0900

    style : SideBar 디자인 작업

--- src/assets/icon/icon.js
+++ src/assets/icon/icon.js
@@ -1,7 +1,28 @@
-import {TfiPencilAlt} from "react-icons/tfi"
+import { TfiPencilAlt } from "react-icons/tfi";
+
+import {
+  HiChevronLeft,
+  HiOutlineLink,
+  HiOutlinePaperAirplane,
+  HiOutlineTicket,
+  HiOutlineCalculator
+} from "react-icons/hi";
+
+import { RiCalendarCheckLine } from "react-icons/ri";
+import { SlPlane ,SlBag } from "react-icons/sl";
+import { FaDoorOpen } from "react-icons/fa";
 
 const Icons = {
-    TfiPencilAlt
-}
+  TfiPencilAlt,
+  HiChevronLeft,
+  HiOutlineLink,
+  HiOutlinePaperAirplane,
+  RiCalendarCheckLine,
+  SlPlane,
+  HiOutlineTicket,
+  HiOutlineCalculator,
+  SlBag,
+  FaDoorOpen
+};
 
-export default Icons
+export default Icons;
