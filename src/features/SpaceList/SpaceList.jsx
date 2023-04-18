25ac98156233ea5df661300d698af40c412a4f8c
imsmallgirl <d__wls_@naver.com>
2023-04-18 18:41:33 +0900

    Fixed : 불필요한 코드 제거

--- src/features/SpaceList/SpaceList.jsx
+++ src/features/SpaceList/SpaceList.jsx
@@ -24,7 +24,6 @@ const SpaceListContent = styled.section`
     justify-content: right;
     margin-bottom: 50px;
   }
-
   div {
     h2 {
       font-size: 1.5rem;
@@ -34,43 +33,13 @@ const SpaceListContent = styled.section`
   }
 `;
 
-const planList = [
-  {
-    id: 0,
-    title: "제주도 여행 가즈아",
-    image: jejuImage,
-    startDate: "2023-03-18",
-    lastDate: "2023-03-20",
-    location: "제주도",
-    members: ["유리", "철수", "짱구"],
-  },
-  {
-    id: 1,
-    title: "베트남으로 떠나요!",
-    image: betImage,
-    startDate: "2023-04-08",
-    lastDate: "2023-04-12",
-    location: "베트남 다낭",
-    members: ["맹구", "훈이", "진이", "동동이"],
-  },
-  {
-    id: 2,
-    title: "강릉에서 회만 먹는 여행",
-    image: gangnenunImage,
-    startDate: "2023-04-18",
-    lastDate: "2023-04-19",
-    location: "강원도 강릉시",
-    members: ["희선", "길동", "지선", "유림"],
-  },
-];
-
 function SpaceList() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
   const [isAddModal, setIsAddModal] = useState(false);
 
-  // const { isAuth } = useSelector((state) => state.auth);
+  const { isAuth } = useSelector((state) => state.auth);
   const { signUpSuccess } = useSelector((state) => state.sign);
   const { currentTravelingList, currentEndTravelList, isTravelLoading } =
     useSelector((state) => state.travel);
@@ -155,7 +124,6 @@ function SpaceList() {
           </div>
         </SpaceListContent>
       </Container>
-      {/* {isAddModal ? <AddSpaceModal setIsAddModal={setIsAddModal} /> : null} */}
       {isAddModal ? (
         <Modal setIsAddModal={setIsAddModal}>
           <AddSpaceModal setIsAddModal={setIsAddModal} />
