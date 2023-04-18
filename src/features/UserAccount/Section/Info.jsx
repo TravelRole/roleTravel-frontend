be613983c07cdcd924cf3c9d4fa2e3b7d05ced1a
Chanwoo-Jeong <lordroad38@naver.com>
2023-04-18 20:30:21 +0900

    [edit] : 채윤님 import 주석처리

--- src/features/UserAccount/Section/Info.jsx
+++ src/features/UserAccount/Section/Info.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { validation } from './validation';
+// import { validation } from './validation';
 import { useNavigate } from "react-router-dom";
 import { Button, Container, Input, InputContainer, Content } from "./Styles";
 import { useDispatch } from "react-redux";
