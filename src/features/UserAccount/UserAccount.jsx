cc58065acf53db297079ffd286e7ae45d1716eb1
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-04-19 19:06:42 +0900

    feat: google 아이콘 추가 및 헤더 섹션 변경

--- src/features/UserAccount/UserAccount.jsx
+++ src/features/UserAccount/UserAccount.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Info from './Section//Info'
 import ChangePassword from './Section/ChangePassword'
-import HeaderSection from "./Section/HeaderSection";
 import { Nav, Tab, ContentWrap, Section, Profile, Avatar } from "./Section/Styles";
 import { useDispatch } from "react-redux";
 import { getLoggedInfo } from "./LoggedUserSlice";
+import Header from '../layout/Header';
+import GoogleIcon from '@mui/icons-material/Google';
 
 
 const UserAccount = () => {
@@ -17,7 +18,7 @@ const UserAccount = () => {
 
   return (
     <>
-      <HeaderSection />
+      <Header />
       <Nav>
         <Tab color={!clicked ? '#A7A7A7' : '#3884FD'} onClick={() => setClicked(true)}>회원정보 수정</Tab>
         <Tab color={clicked ? '#A7A7A7' : '#3884FD'} onClick={() => setClicked(false)}>비밀번호 변경</Tab>
@@ -26,7 +27,7 @@ const UserAccount = () => {
         <Section>
           <Profile>
             <Avatar src="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80" alt="avatar" />
-            <p>홍길동 님</p>
+            <p>홍길동 님 <GoogleIcon /></p>
           </Profile>
         </Section>
         <Section>
