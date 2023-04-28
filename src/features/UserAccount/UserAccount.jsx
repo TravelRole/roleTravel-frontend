fb8b1f6a8c075cf8b79f3f24f075a21f0c55ab35
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-04-28 19:46:27 +0900

    feat: api 연결, 이미지 수정 아이콘 추가

--- src/features/UserAccount/UserAccount.jsx
+++ src/features/UserAccount/UserAccount.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Info from './Section//Info'
 import ChangePassword from './Section/ChangePassword'
-import { Nav, Tab, ContentWrap, Section, Profile, Avatar } from "./Section/Styles";
-import { useDispatch } from "react-redux";
+import { Nav, Tab, ContentWrap, Section, Profile, Avatar, EditIcon } from "./Section/Styles";
+import { useDispatch, useSelector } from "react-redux";
 import { getLoggedInfo } from "./LoggedUserSlice";
 import Header from '../layout/Header';
-import GoogleIcon from '@mui/icons-material/Google';
+import Icons from "../../assets/icon/icon";
 
 
 const UserAccount = () => {
   const [clicked, setClicked] = useState(true);
-
   const dispatch = useDispatch();
+  const { presignedUrl, loggedInfo } =
+    useSelector((state) => state.loggedUser);
+  
   useEffect(() => {
     dispatch(getLoggedInfo());
   }, [dispatch]);
+
+  useEffect(() => {
+    
+  })
+
+  const changeAvatarHandler = () => {
+    console.log('이미지 변경 성공')
+  };
 
   return (
     <>
@@ -26,8 +36,16 @@ const UserAccount = () => {
       <ContentWrap>
         <Section>
           <Profile>
-            <Avatar src="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80" alt="avatar" />
-            <p>홍길동 님 <GoogleIcon /></p>
+            <div>
+              <Avatar src="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80" alt="avatar" />
+              <EditIcon onClick={changeAvatarHandler}>
+                  <Icons.HiOutlinePencilAlt color="black" style={{ postiion: 'absolute', width: '15.68px', height: '15.68px'}} />
+              </EditIcon>
+            </div>
+            <div>
+              <p> 홍길동 님 </p>
+              <span>{loggedInfo.provider === 'google' ? <Icons.FcGoogle /> : <></>}</span>
+            </div>
           </Profile>
         </Section>
         <Section>
