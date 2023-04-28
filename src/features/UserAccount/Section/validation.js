9a82a54f2af92ea2db403048301d8ea688a162e0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-04-28 19:47:10 +0900

    feat: validation 수정

--- src/features/UserAccount/Section/validation.js
+++ src/features/UserAccount/Section/validation.js
@@ -7,53 +7,44 @@ export const nameValidation = (value) => {
 
 
 export const dateValidation = (value) => {
+  let success = false;
+  let error = '';
+
   const numberReg = /^[0-9/]*$/g;
+  const thisYear = new Date().getFullYear();
+
   if (!numberReg.test(value)) {
-    return {
-      success: false,
-      error: '* 숫자 외의 문자는 입력할 수 없습니다.'
-    }
-  } else if (value.length === 4) {
-    let thisYear = new Date().getFullYear();
-    if (value > thisYear) {
-      return {
-        success: false,
-        error: '* 잘못된 형식입니다.'
-      }
-    } else {
-      return {
-        success: true,
-        error: ''
-      }
-    }
+      success = false;
+      error = '* 숫자 외의 문자는 입력할 수 없습니다.';
+  } else if (value.length === 4 && value > thisYear) {
+      success = false;
+      error = '* 잘못된 형식입니다.';
   } else if (value.length === 7) {
      if (value.substr(5, 2) < 1 || value.substr(5, 2) > 12) {
-      return {
-        success: false,
-        error: '* 잘못된 형식입니다.'
-      }
-    } else {
-      return {
-        success: true,
-        error: ''
-      }
+      success = false;
+      error = '* 잘못된 형식입니다.';
     }
   } else if (value.length === 10) {
     if (value.substr(8, 2) < 1 || value.substr(8, 2) > 31) {
-      return {
-        success: false,
-        error: '* 잘못된 형식입니다.'
-      }
-    } else {
-      return {
-        success: true,
-        error: ''
-      }
+      success = false;
+      error = '* 잘못된 형식입니다.';
     }
   } else {
-    return {
-      success: true,
-      error: ''
-    }
+    success = true;
+    error = '';
+  }
+
+  return {
+    success: success,
+    error: error
   }
 }
+
+
+export const switched = (name, value) => {
+  switch (name) {
+    case 'name': return nameValidation(value)
+    case 'date': return dateValidation(value);
+    default: return;
+  }
+};
