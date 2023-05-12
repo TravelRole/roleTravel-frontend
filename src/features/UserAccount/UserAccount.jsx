0b72f19c1ec7f375f1c046c1da5042e0770a7969
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-05-12 12:22:39 +0900

    refactor: userAccount 페이지 코드 정리 및 이미지 여부에 따라 사진 추가

--- src/features/UserAccount/UserAccount.jsx
+++ src/features/UserAccount/UserAccount.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
-import Info from './Section//Info'
-import ChangePassword from './Section/ChangePassword'
-import { Nav, Tab, ContentWrap, Section, Profile, Avatar, EditIcon } from "./Section/Styles";
+import Info from "./Section//Info";
+import ChangePassword from "./Section/ChangePassword";
+import {
+  Nav,
+  Tab,
+  ContentWrap,
+  Section,
+  Profile,
+  Avatar,
+  EditIcon
+} from "./Section/Styles";
 import { useDispatch, useSelector } from "react-redux";
 import { getLoggedInfo } from "./LoggedUserSlice";
-import Header from '../layout/Header';
+import Header from "../layout/Header";
 import Icons from "../../assets/icon/icon";
 import AddImageModal from "./Modal/AddImageModal";
 import { useNavigate } from "react-router-dom";
@@ -12,17 +20,18 @@ import { useNavigate } from "react-router-dom";
 const UserAccount = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { loggedInfo } =
-    useSelector((state) => state.loggedInUser);
+  const { loggedInfo } = useSelector((state) => state.loggedInUser);
   const [clicked, setClicked] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+  const [image, setImage] = useState("");
 
   useEffect(() => {
     if (!localStorage.getItem("accessToken")) {
-      navigate('/login');
+      navigate("/login");
       return;
+    } else {
+      dispatch(getLoggedInfo());
     }
-    dispatch(getLoggedInfo());
   }, [dispatch, navigate]);
 
   const clickHandler = () => {
@@ -33,32 +42,64 @@ const UserAccount = () => {
     <>
       <Header />
       <Nav>
-        <Tab color={!clicked ? '#A7A7A7' : '#3884FD'} onClick={() => setClicked(true)}>회원정보 수정</Tab>
-        <Tab color={clicked ? '#A7A7A7' : '#3884FD'} onClick={() => setClicked(false)}>비밀번호 변경</Tab>
+        <Tab
+          color={!clicked ? "#A7A7A7" : "#3884FD"}
+          onClick={() => setClicked(true)}
+        >
+          회원정보 수정
+        </Tab>
+        <Tab
+          color={clicked ? "#A7A7A7" : "#3884FD"}
+          onClick={() => setClicked(false)}
+        >
+          비밀번호 변경
+        </Tab>
       </Nav>
       <ContentWrap>
         <Section>
           <Profile>
             <div>
-              <Avatar src={loggedInfo ? loggedInfo.profile : 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1635&q=80'} alt="avatar" />
+              {loggedInfo && loggedInfo.profile ? (
+                <Avatar
+                  src={loggedInfo.profile}
+                  alt="avatar"
+                />
+              ) : (
+                <Avatar
+                  src={""}
+                  style={{ backgroundColor: "pink" }}
+                  width="100%"
+                />
+              )}
               <EditIcon onClick={clickHandler}>
-                  <Icons.HiOutlinePencilAlt color="black" style={{ postiion: 'absolute', width: '15.68px', height: '15.68px'}} />
+                <Icons.HiOutlinePencilAlt
+                  color="black"
+                  style={{
+                    postiion: "absolute",
+                    width: "15.68px",
+                    height: "15.68px",
+                    marginTop: "-8px"
+                  }}
+                />
               </EditIcon>
             </div>
             <div>
               <p> {loggedInfo?.name} 님 </p>
-              {/* <span>{loggedInfo.provider === 'google' ? <Icons.FcGoogle /> : <></>}</span> */}
             </div>
           </Profile>
         </Section>
-        <Section>
-          {clicked ? <Info /> : <ChangePassword />}
-        </Section>
+        <Section>{clicked ? <Info /> : <ChangePassword />}</Section>
         <Section></Section>
       </ContentWrap>
       {isOpen ? (
-        <AddImageModal setIsOpen={setIsOpen} />
-      ) : ''}
+        <AddImageModal
+          setIsOpen={setIsOpen}
+          image={image}
+          setImage={setImage}
+        />
+      ) : (
+        ""
+      )}
     </>
   );
 };
