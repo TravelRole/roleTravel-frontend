f377c57b9027ca31c7947699fd7f538791afa062
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-05-17 22:22:23 +0900

    feat: delete modal change styles from px to rem

--- src/features/Role/Essentials/components/CheckDeleteModal/Style.js
+++ src/features/Role/Essentials/components/CheckDeleteModal/Style.js
@@ -12,17 +12,17 @@ export const Blur = styled.div`
 `;
 
 export const ModalWrapper = styled.div`
-  width: 418px;
-  height: 233px;
-  box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.12);
-  border-radius: 16px;
+  width: 41.8rem;
+  height: 23.3rem;
+  box-shadow: 0rem 2rem 2rem rgba(0, 0, 0, 0.08), 0rem 0rem 0.2rem rgba(0, 0, 0, 0.12);
+  border-radius: 1.6rem;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: #fff;
   border: none;
-  padding: 20px 24px 20px 24px;
+  padding: 2rem 2.4rem 2rem 2.4rem;
 `;
 
 export const Section = styled.div`
@@ -31,22 +31,22 @@ export const Section = styled.div`
   display: flex;
 
   &:nth-child(1) {
-    margin-top: 20px;
+    margin-top: 2rem;
     flex-direction: column;
     align-items: center;
-    margin-bottom: 25px;
+    margin-bottom: 2.5rem;
   }
 
   &:nth-child(2) {
     justify-content: center;
-    gap: 10px;
+    gap: 1rem;
   }
 `;
 
 export const Error = styled.div`
-  width: 35px;
-  height: 35px;
-  border: 3px solid #ffc759;
+  width: 3.5rem;
+  height: 3.5rem;
+  border: 0.3rem solid #ffc759;
   border-radius: 100%;
 `;
 
@@ -54,8 +54,8 @@ export const Title = styled.h1`
   font-size: 2rem;
   font-weight: 600;
   color: #333333;
-  margin-top: 15px;
-  margin-bottom: 16px;
+  margin-top: 1.5rem;
+  margin-bottom: 1.6rem;
 `;
 
 export const Span = styled.span`
@@ -65,13 +65,13 @@ export const Span = styled.span`
 `;
 
 export const Button = styled.button`
-  width: 180px;
-  height: 45px;
+  width: 18rem;
+  height: 4.5rem;
   font-size: 1.6rem;
   font-weight: 500;
-  border-radius: 8px;
+  border-radius: 0.8rem;
   cursor: pointer;
   background: ${(props) => (props.color === "blue" ? "#3884fd" : "#fff")};
-  border: ${(props) => (props.color === "blue" ? "none" : "1px solid #c4c4c4")};
+  border: ${(props) => (props.color === "blue" ? "none" : "0.1rem solid #c4c4c4")};
   color: ${(props) => (props.color === "blue" ? "#fafafa" : "#333333")};
 `;
