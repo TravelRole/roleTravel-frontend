2a3d8ca16254fbfd2c142798d4bd7a0fc57c8cea
Chanwoo-Jeong <lordroad38@naver.com>
2023-05-24 01:50:25 +0900

    [refactor]: AmountContainer 컴포넌트 분리

--- src/features/Role/Account/Account.jsx
+++ src/features/Role/Account/Account.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import React, { useState } from "react";
-import Icons from "../../../assets/icon/icon";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+import AmountContainer from "./layout/AmountContainer";
 
 const Wrapper = styled.div`
   display: flex;
@@ -37,68 +37,6 @@ const PageHeader = styled.header`
   padding: 7.8rem 0 3rem 6rem;
   font-size: 3rem;
   font-weight: 600;
-`;
-
-const AmountContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  gap: 2.5rem;
-  padding: 0 6rem;
-  padding-bottom: 6rem;
-`;
-const AmountBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  width: 100%;
-  height: 14.4rem;
-  padding: 2.5rem;
-  position: relative;
-
-  background-color: white;
-  border-radius: 1.6rem;
-
-  header {
-    color: #8b8b8b;
-    font-weight: 600;
-    font-size: 1.6rem;
-  }
-  p {
-    color: #141414;
-    font-size: 3rem;
-    font-weight: 600;
-  }
-
-  span {
-    color: #a7a7a7;
-    font-size: 1.4rem;
-    font-weight: 500;
-  }
-
-  button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: none;
-    background-color: white;
-    position: absolute;
-    right: 0;
-    padding: 0 2.4rem 0 0;
-    cursor: pointer;
-
-    span,
-    svg {
-      font-size: 1.6rem;
-      font-weight: 500;
-      margin-right: 0.8rem;
-      color: #a7a7a7;
-    }
-    svg {
-      margin: 0;
-    }
-  }
 `;
 
 const AmonutDetailContainer = styled.div``;
@@ -160,26 +98,7 @@ function Account() {
     <>
       <Wrapper>
         <PageHeader>회계 목록</PageHeader>
-        <AmountContainer>
-          <AmountBox>
-            <header>공통 경비</header>
-            <p>1,500,000원</p>
-            <span>수정하기를 클릭해 공동 경비를 입력해보세요!</span>
-            <button>
-              <span>수정하기</span> <Icons.FaChevronRight />{" "}
-            </button>
-          </AmountBox>
-          <AmountBox>
-            <header>남은 경비</header>
-            <p>340,000원</p>
-            <span>잔액을 파악해 경비를 효율적으로 관리해 보세요!</span>
-          </AmountBox>
-          <AmountBox>
-            <header>여행 총 지출 금액</header>
-            <p>1,160,000원</p>
-            <span>일자별 지출 금액을 모두 합산한 금액입니다.</span>
-          </AmountBox>
-        </AmountContainer>
+        <AmountContainer />
         <AmonutDetailContainer>
           <AmountDetailHeader>
             <header>일자별 지출 금액</header>
