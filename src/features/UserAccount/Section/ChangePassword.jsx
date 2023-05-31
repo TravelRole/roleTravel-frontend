f72c853d8c8dd21a232f21c21661e191dc2a6f79
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-05-31 21:51:21 +0900

    remove: remove navigate

--- src/features/UserAccount/Section/ChangePassword.jsx
+++ src/features/UserAccount/Section/ChangePassword.jsx
@@ -11,20 +11,20 @@ import { toast } from "react-toastify";
 const ChangePassword = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [show, setShow] = useState({ password: false, newPassword: false})
-  const [inputs, setInputs] = useState({ newPassword: '', password: ''});
+  const [show, setShow] = useState({ password: false, newPassword: false });
+  const [inputs, setInputs] = useState({ newPassword: "", password: "" });
   const [errors, setErrors] = useState({
-    newPassword: '',
-    password: '',
-  })
+    newPassword: "",
+    password: ""
+  });
 
   const changeHandler = (e) => {
-    const {value, name} = e.target;
+    const { value, name } = e.target;
     if (value.length > 16) {
-      setErrors({...errors, [name]: '* 16자를 넘을 수 없습니다.'})
+      setErrors({ ...errors, [name]: "* 16자를 넘을 수 없습니다." });
     } else {
-      setErrors({...errors, [name]: ''})
-      setInputs({...inputs, [name]: value})
+      setErrors({ ...errors, [name]: "" });
+      setInputs({ ...inputs, [name]: value });
     }
   };
 
@@ -32,36 +32,45 @@ const ChangePassword = () => {
     const pattern = /^[a-zA-Z0-9-_!]{8,16}$/;
 
     if (!pattern.test(inputs.password)) {
-      setErrors({...errors, password: '* 8~16자 영문, 숫자, 특수문자를 사용하세요.'})
+      setErrors({
+        ...errors,
+        password: "* 8~16자 영문, 숫자, 특수문자를 사용하세요."
+      });
       return;
     }
 
     if (!pattern.test(inputs.newPassword)) {
-      setErrors({...errors, newPassword: '* 8~16자 영문, 숫자, 특수문자를 사용하세요.'})
+      setErrors({
+        ...errors,
+        newPassword: "* 8~16자 영문, 숫자, 특수문자를 사용하세요."
+      });
       return;
     }
 
-    dispatch(changePassword({
-      password: inputs.password,
-      newPassword: inputs.newPassword,
-      newPasswordCheck: inputs.newPassword
-    })).then((res) => {
+    dispatch(
+      changePassword({
+        password: inputs.password,
+        newPassword: inputs.newPassword,
+        newPasswordCheck: inputs.newPassword
+      })
+    ).then((res) => {
       if (res.meta.requestStatus === "fulfilled") {
         setTimeout(() => {
-          toast.success('비밀번호 수정이 완료되었습니다.', {
-            position: 'top-center',
+          toast.success("비밀번호 수정이 완료되었습니다.", {
+            position: "top-center",
             autoClose: 2000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
-            draggable: false,
-            onClose: () => navigate(`/home`)
+            draggable: false
           });
         }, 1);
       } else {
-        toast.error('올바르지 않은 정보입니다. 다시 확인해주세요.\n비밀번호는 (8~16자의 영문, 숫자, 특수기호(!), (_) (-) 만 사용 가능합니다.)');
+        toast.error(
+          "올바르지 않은 정보입니다. 다시 확인해주세요.\n비밀번호는 (8~16자의 영문, 숫자, 특수기호(!), (_) (-) 만 사용 가능합니다.)"
+        );
       }
-    })
+    });
   };
 
   return (
@@ -70,7 +79,13 @@ const ChangePassword = () => {
         <h1>비밀번호 변경</h1>
         <div width="7.6rem">
           <p>비밀번호는 8~16자 영문, 숫자, 특수문자를 사용할 수 있습니다.</p>
-          <p>비밀번호는 <span style={{ color: '#3884FD', fontSize: '1.7rem'}}>주기적(최소 6개월)</span>으로 변경해 주시기 바랍니다.</p>
+          <p>
+            비밀번호는{" "}
+            <span style={{ color: "#3884FD", fontSize: "1.7rem" }}>
+              주기적(최소 6개월)
+            </span>
+            으로 변경해 주시기 바랍니다.
+          </p>
         </div>
       </Content>
       <Content>
@@ -84,16 +99,18 @@ const ChangePassword = () => {
             maxLength={11}
             error={errors.password.length > 0}
             helperText={errors.password ? errors.password : ""}
-            type={show.password ? 'text' : 'password'}
+            type={show.password ? "text" : "password"}
             InputProps={{
               endAdornment: (
-                <IconButton onClick={() => setShow({...show, password: !show.password})}>
+                <IconButton
+                  onClick={() => setShow({ ...show, password: !show.password })}
+                >
                   {show.password ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
               ),
-              maxLength: 16,
+              maxLength: 16
             }}
-            />
+          />
         </InputContainer>
         <InputContainer>
           <TextField
@@ -105,16 +122,20 @@ const ChangePassword = () => {
             maxLength={11}
             error={errors.newPassword.length > 0}
             helperText={errors.newPassword ? errors.newPassword : ""}
-            type={show.newPassword ? 'text' : 'password'}
+            type={show.newPassword ? "text" : "password"}
             InputProps={{
               endAdornment: (
-                <IconButton onClick={() => setShow({...show, newPassword: !show.newPassword})}>
+                <IconButton
+                  onClick={() =>
+                    setShow({ ...show, newPassword: !show.newPassword })
+                  }
+                >
                   {show.newPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
               ),
-              maxLength: 16,
+              maxLength: 16
             }}
-            />
+          />
         </InputContainer>
       </Content>
       <Content>
