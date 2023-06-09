26363c0a6da2b6a7cc1ad88b0f8afc6612b9da3c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-06-10 00:20:21 +0900

    fix: fix errors about birth validation

--- src/features/UserAccount/Section/validation.js
+++ src/features/UserAccount/Section/validation.js
@@ -1,47 +1,69 @@
 export const nameValidation = (value) => {
   return {
-      success: true,
-      error: ''
-    }
-}
-
+    success: true,
+    error: ""
+  };
+};
 
 export const dateValidation = (value) => {
   let success = false;
-  let error = '';
+  let error = "";
 
   const numberReg = /^[0-9/]*$/g;
-  const thisYear = new Date().getFullYear();
 
   if (!numberReg.test(value)) {
-      success = false;
-      error = '* 숫자 외의 문자는 입력할 수 없습니다.';
-  } else if (value.length === 4 && value > thisYear) {
-      success = false;
-      error = '* 잘못된 형식입니다.';
-  } else if (value.length === 7) {
-     if (value.substr(5, 2) < 1 || value.substr(5, 2) > 12) {
-      success = false;
-      error = '* 잘못된 형식입니다.';
-    }
-  } else if (value.length === 10) {
-    if (value.substr(8, 2) < 1 || value.substr(8, 2) > 31) {
-      success = false;
-      error = '* 잘못된 형식입니다.';
-    }
+    success = false;
+    error = "* 숫자 외의 문자는 입력할 수 없습니다.";
+  } else if (value.length > 2 && (!["1", "2"].includes(value[0]))) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
+  } else if (value.length > 2 && !["9", "0"].includes(value[1])) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
+  } else if (value.length > 5 && !["0", "1"].includes(value[5])) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
+  } else if (
+    value.length > 6 &&
+    (Number(value.slice(5, 7)) > 12 || Number(value.slice(5, 7)) < 1)
+  ) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
+  } else if (
+    value.length > 8 &&
+    !["1", "3", "5", "7", "8", "10", "12"].includes(
+      Number(value.slice(5, 7))
+    ) &&
+    (Number(value.slice(8, 10)) > 31 || Number(value.slice(8, 10)) < 1)
+  ) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
+  } else if (
+    value.length > 8 &&
+    !["4", "6", "9", "11"].includes(Number(value.slice(5, 7))) &&
+    (Number(value.slice(8, 10)) > 30 || Number(value.slice(8, 10)) < 1)
+  ) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
+  } else if (
+    value.length > 8 &&
+    !["2"].includes(Number(value.slice(5, 7))) &&
+    (Number(value.slice(8, 10)) > 29 || Number(value.slice(8, 10)) < 1)
+  ) {
+    success = false;
+    error = "* 잘못된 날짜 형식입니다.";
   } else {
     success = true;
-    error = '';
+    error = "";
   }
 
   return {
     success: success,
     error: error
-  }
-}
-
+  };
+};
 
 export const switched = (name, value) => {
-  if (name === 'nickname') return nameValidation(value)
-  else if (name === 'date') return dateValidation(value);
+  if (name === "nickname") return nameValidation(value);
+  else if (name === "date") return dateValidation(value);
 };
